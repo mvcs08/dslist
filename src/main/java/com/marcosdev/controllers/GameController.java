652ee16f0af45262a469432718940f424cb04572
Marcos <mdominique2011@gmail.com>
2025-01-23 01:47:47 -0300

    Get game by ID

--- src/main/java/com/marcosdev/controllers/GameController.java
+++ src/main/java/com/marcosdev/controllers/GameController.java
@@ -1,9 +1,12 @@
 package com.marcosdev.controllers;
 
+import com.marcosdev.dto.GameDTO;
 import com.marcosdev.dto.GameMinDTO;
 import com.marcosdev.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,11 @@ public class GameController {
 
     @GetMapping
     public List<GameMinDTO> findAll(){
-        return gameService.getAllGames();
+        return gameService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/marcosdev/services/GameService.java
+++ src/main/java/com/marcosdev/services/GameService.java
@@ -1,9 +1,16 @@
 package com.marcosdev.services;
 
+import com.marcosdev.dto.GameDTO;
 import com.marcosdev.dto.GameMinDTO;
+import com.marcosdev.entities.Game;
 import com.marcosdev.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
 
 import java.util.List;
 
@@ -12,8 +19,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public List<GameMinDTO> getAllGames() {
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
-
+    }
+    @Transactional(readOnly = true) // Transactional garante que a operação com o banco ocorrerá segundo o princípio ACID
+    public GameDTO findById(long id) {
+        var game = gameRepository.findById(id).get();
+        return new GameDTO(game);
     }
 }
