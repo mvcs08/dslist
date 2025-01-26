2c174ea3b798a5f7e24285804fb0f751581df440
Marcos <mdominique2011@gmail.com>
2025-01-25 21:52:44 -0300

    Approve ok

--- src/main/java/com/marcosdev/dto/GameMinListDTO.java
+++ src/main/java/com/marcosdev/dto/GameMinListDTO.java
@@ -5,6 +5,6 @@ import com.marcosdev.projections.GameMinProjection;
 public record GameMinListDTO(Long id, String title, Integer year, String imgUrl, String shortDescription, Integer position) {
 
     public GameMinListDTO(GameMinProjection projection) {
-        this(projection.getId(), projection.getTitle(), projection.getYear(), projection.getImgUrl(), projection.getShortDescription(), projection.getPosition());
+        this(projection.getId(), projection.getTitle(), projection.getGameYear(), projection.getImgUrl(), projection.getShortDescription(), projection.getPosition());
     }
 }
